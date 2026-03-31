f5d6b736ff32c2e0b1201a57cf16347a6c38e874
trionoaja <trionoaja@gmail.com>
2026-03-31 18:35:42 +0700

    update

---
+++ application/controllers/kategori.php
@@ -0,0 +1,15 @@
+<?php
+
+class kategori extends CI_Controller{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('kategori_model');
+    }
+    public function index(){
+
+        $data['kategori']= $this->kategori_model->get_all();
+        $this->load->view('kategori/index', $data);
+    }
+}
