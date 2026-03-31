bc40270865bcc0288239db2e80c89b430e9fc245
trionoaja <trionoaja@gmail.com>
2026-03-31 19:17:57 +0700

    Update kategori.php

--- application/controllers/kategori.php
+++ application/controllers/kategori.php
@@ -10,6 +10,10 @@ class kategori extends CI_Controller{
     public function index(){
 
         $data['kategori']= $this->kategori_model->get_all();
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('templates/topbar');
         $this->load->view('kategori/index', $data);
+        $this->load->view('templates/footer');
     }
 }
